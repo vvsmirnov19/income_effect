d9e3d67e5772b374fba115051881c72a388874c9
Valeriy Smirnov <valeriy.5mirnov@yandex.ru>
2024-09-05 20:06:19 +0700

    add docstrings and annotations

--- numerics.py
+++ numerics.py
@@ -1,14 +1,18 @@
+"""Provide class for data input."""
 import sympy as sp
 
 
 class Numerics:
+    """Get data for calculation from user's input."""
 
     def get_budget(self) -> None:
+        """Transfers budget into the class field."""
         self.budget = int(input(
             'Enter budget: ')
             )
 
     def get_prices(self) -> None:
+        """Transfers prices into the class field."""
         self.prices = [
             float(price) for price in input(
                 'Enter prices w/o commas: '
@@ -16,6 +20,7 @@ class Numerics:
             ]
 
     def get_new_prices(self) -> None:
+        """Transfers new prices into the class field."""
         self.new_prices = [
             float(price) for price in input(
                 'Enter new prices w/o commas: '
@@ -23,6 +28,7 @@ class Numerics:
             ]
 
     def get_utility_function(self) -> None:
+        """Transfers utility function into the class field."""
         str_expr = input(
             'Enter utility expression with "x" and "y": '
             )
